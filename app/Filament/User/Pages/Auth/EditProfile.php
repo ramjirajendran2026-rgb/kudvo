5172590ec50f64eb1ac13627440499e01bd608b3
Iliyas M <im.smiliyas@gmail.com>
2024-02-17 17:03:45 +0530

    fix: password update issue

--- app/Filament/User/Pages/Auth/EditProfile.php
+++ app/Filament/User/Pages/Auth/EditProfile.php
@@ -6,7 +6,6 @@ use App\Forms\UserForm;
 use App\Models\User;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Component;
-use Filament\Forms\Get;
 use Filament\Pages\Auth\EditProfile as BasePage;
 use Filament\Panel;
 
@@ -21,21 +20,6 @@ class EditProfile extends BasePage
     {
         return UserForm::emailComponent()
             ->disabled(condition: fn (?User $user): bool => $user?->hasVerifiedEmail());
-    }
-
-    protected function getPasswordFormComponent(): Component
-    {
-        return UserForm::passwordComponent()
-            ->autocomplete(autocomplete: 'new-password')
-            ->dehydrated(condition: fn ($state): bool => filled($state))
-            ->live(debounce: 500)
-            ->required(condition: false);
-    }
-
-    protected function getPasswordConfirmationFormComponent(): Component
-    {
-        return UserForm::passwordConfirmationComponent()
-            ->visible(condition: fn (Get $get): bool => filled(value: $get(path: 'password')));
     }
 
     public static function getWithoutRouteMiddleware(Panel $panel): string|array
