12a5f61d78510d9d410c9d74783bbedaaeef36c8
Iliyas M <im.smiliyas@gmail.com>
2024-02-20 12:18:41 +0530

    cs

--- database/migrations/2024_02_08_163259_create_election_monitor_tokens_table.php
+++ database/migrations/2024_02_08_163259_create_election_monitor_tokens_table.php
@@ -8,24 +8,27 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('election_monitor_tokens', function (Blueprint $table) {
-            $table->id();
+        Schema::create(
+            table: 'election_monitor_tokens',
+            callback: function (Blueprint $table): void {
+                $table->id();
 
-            $table->uuid(column: 'key')->unique();
-            $table->timestamp(column: 'activated_at')->nullable();
-            $table->ipAddress()->nullable();
-            $table->text(column: 'user_agent')->nullable();
+                $table->uuid(column: 'key')->unique();
+                $table->timestamp(column: 'activated_at')->nullable();
+                $table->ipAddress()->nullable();
+                $table->text(column: 'user_agent')->nullable();
 
-            $table->foreignIdFor(model: Election::class)
-                ->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+                $table->foreignIdFor(model: Election::class)
+                    ->constrained()->cascadeOnUpdate()->cascadeOnDelete();
 
-            $table->timestamps();
-            $table->softDeletes();
-        });
+                $table->timestamps();
+                $table->softDeletes();
+            },
+        );
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('election_monitor_tokens');
+        Schema::dropIfExists(table: 'election_monitor_tokens');
     }
 };
