f85b53524dc847a889d9fa4a1c85e441030d3caf
Iliyas M <im.smiliyas@gmail.com>
2024-03-28 11:05:48 +0530

    fix: dashboard state

--- app/Policies/ElectionPolicy.php
+++ app/Policies/ElectionPolicy.php
@@ -68,7 +68,6 @@ class ElectionPolicy
     public function publish(User $user, Election $election): bool
     {
         return $election->is_draft &&
-            $election->is_paid &&
             $election->isTimingConfigured() &&
             filled($election->preference) &&
             ($election->electors_count ?? $election->loadCount(relations: ['electors'])->electors_count) > 0 &&
