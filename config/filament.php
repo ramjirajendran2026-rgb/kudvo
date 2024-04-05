2503f05c9856c86b76bf25b8b3ff4d138d2e6d50
Iliyas M <im.smiliyas@gmail.com>
2024-04-05 18:06:26 +0530

    pusher broadcast integration

--- config/filament.php
+++ config/filament.php
@@ -17,17 +17,15 @@ return [
     'broadcasting' => [
 
          'echo' => [
-             'broadcaster' => 'reverb',
-             'key' => env('VITE_REVERB_APP_KEY'),
-//             'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
-             'wsHost' => env('VITE_REVERB_HOST'),
-             'wsPort' => env('VITE_REVERB_PORT'),
-             'wssPort' => env('VITE_REVERB_PORT'),
-             'forceTLS' => env('VITE_REVERB_SCHEME', 'https') === 'https',
-             'enabledTransports' => ['ws', 'wss'],
+             'broadcaster' => 'pusher',
+             'key' => env('VITE_PUSHER_APP_KEY'),
+             'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
+             'wsHost' => env('VITE_PUSHER_HOST'),
+             'wsPort' => env('VITE_PUSHER_PORT'),
+             'wssPort' => env('VITE_PUSHER_PORT'),
 //             'authEndpoint' => '/api/v1/broadcasting/auth',
-//             'disableStats' => true,
-//             'encrypted' => true,
+             'disableStats' => true,
+             'encrypted' => true,
          ],
 
     ],
