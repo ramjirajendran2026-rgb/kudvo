1b842fe91ab3558c701b18f22b1874ad3839c832
Iliyas M <im.smiliyas@gmail.com>
2024-05-28 11:41:46 +0530

    remove factory dependency from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,18 +4,17 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Nnjeim\World\Actions\SeedAction;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()
-            ->create(attributes: [
-                'name' => 'Iliyas M',
-                'email' => 'iliyas.m@inodesys.com',
-                'password' => 'password',
-            ]);
+        User::create(attributes: [
+            'name' => 'Iliyas M',
+            'email' => 'iliyas.m@inodesys.com',
+            'password' => 'password',
+            'email_verified_at' => now(),
+        ]);
 
         $this->call([
             WorldSeeder::class,
