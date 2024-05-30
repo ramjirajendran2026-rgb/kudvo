74ae339c5f8097c0be64270d096e866c7c99ff50
Iliyas M <im.smiliyas@gmail.com>
2024-05-30 10:21:46 +0530

    update cors paths

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'build/*', 'css/*', 'js/*', 'img/*', 'video/*', 'vendor/*', 'favicon.ico'],
 
     'allowed_methods' => ['*'],
 
