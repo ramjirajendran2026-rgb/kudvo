5ef6713b0109a2081777cf369300d9aeba7c1894
Iliyas M <im.smiliyas@gmail.com>
2024-06-18 15:54:36 +0530

    fix: exception on booth elector revoked by admin

--- app/Filament/Election/Pages/Concerns/InteractsWithElector.php
+++ app/Filament/Election/Pages/Concerns/InteractsWithElector.php
@@ -2,34 +2,17 @@
 
 namespace App\Filament\Election\Pages\Concerns;
 
-use App\Facades\Kudvo;
-use App\Models\Election;
 use App\Models\Elector;
-use App\Models\Nomination;
 use Filament\Facades\Filament;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\HtmlString;
-use Jenssegers\Agent\Agent;
 use Livewire\Attributes\Locked;
-use function Filament\authorize;
 
 trait InteractsWithElector
 {
-    #[Locked]
-    protected Elector $elector;
-
-    public function bootInteractsWithElector(): void
+    public function getElector(): Elector
     {
         /** @var Elector $elector */
         $elector = Filament::auth()->user();
 
-        $this->elector = $elector;
-    }
-
-    public function getElector(): Elector
-    {
-        return $this->elector;
+        return $elector;
     }
 }
