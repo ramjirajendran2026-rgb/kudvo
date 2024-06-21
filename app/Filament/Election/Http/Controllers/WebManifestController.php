37af7cd0a2eef1f479ab7065bc8dfb953b886cf3
Iliyas M <im.smiliyas@gmail.com>
2024-06-21 16:18:00 +0530

    chore: cs

--- app/Filament/Election/Http/Controllers/WebManifestController.php
+++ app/Filament/Election/Http/Controllers/WebManifestController.php
@@ -4,13 +4,10 @@ namespace App\Filament\Election\Http\Controllers;
 
 use App\Data\WebAppManifestIconData;
 use App\Enums\WebAppManifestDisplay;
-use App\Facades\Kudvo;
 use App\Filament\Election\Pages\Index;
 use App\Http\Controllers\Controller;
 use App\Models\Election;
-use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\Response;
 
 class WebManifestController extends Controller
@@ -24,7 +21,7 @@ class WebManifestController extends Controller
         $data->id = "el/$election->code";
         $data->display = WebAppManifestDisplay::Standalone;
 
-        $data->icons?->map(function (WebAppManifestIconData $icon) use ($election) {
+        $data->icons?->map(function (WebAppManifestIconData $icon) {
             $icon->src = Storage::disk(config('filament.default_filesystem_disk'))->url(path: $icon->src);
 
             return $icon;
