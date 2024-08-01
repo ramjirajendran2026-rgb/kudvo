f8c32c65caeb087bc64218a709d2d000dbfe2a0e
Iliyas M <im.smiliyas@gmail.com>
2024-08-01 15:09:45 +0530

    fix: slider image lazy loading

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -80,7 +80,7 @@
                 class="relative flex flex-col bg-cover md:aspect-[2.23/1] md:bg-var-url lg:flex-row lg:pb-0 lg:pt-0"
             >
                 <img
-                    {{ ! $loop->first ? 'loading="lazy"' : '' }}
+                    {!! ! $loop->first ? 'loading="lazy"' : '' !!}
                     class="absolute inset-0 -z-10 hidden aspect-[2.23/1] object-cover md:block"
                     src="{{ $item->image }}"
                     alt="{{ $item->title }}"
@@ -140,7 +140,7 @@
                     class="inset-y-0 right-0 top-0 z-0 mx-auto block w-full md:hidden md:px-0 lg:absolute lg:mx-0 lg:mb-0 lg:pr-0 xl:px-0"
                 >
                     <img
-                        {{ ! $loop->first ? 'loading="lazy"' : '' }}
+                        {!! ! $loop->first ? 'loading="lazy"' : '' !!}
                         class="block h-[343px] w-full object-cover object-right-bottom [mask-image:_linear-gradient(to_bottom,transparent_0,_black_100px,_black_calc(100%-1px),transparent_100%)] md:hidden"
                         src="{{ $item->image }}"
                         alt="{{ $item->title }}"
