2fa9047558f1aabef987fda7e9477eabd12564e4
Iliyas M <im.smiliyas@gmail.com>
2025-02-17 14:21:39 +0530

    Add custom cancel action to EditSurvey page
    Implemented a custom Alpine.js click handler for the cancel form action in EditSurvey to redirect users back to the resource URL. This ensures a more seamless navigation experience while maintaining consistency across the application.

--- app/Filament/User/Resources/SurveyResource/Pages/EditSurvey.php
+++ app/Filament/User/Resources/SurveyResource/Pages/EditSurvey.php
@@ -3,9 +3,11 @@
 namespace App\Filament\User\Resources\SurveyResource\Pages;
 
 use App\Filament\User\Resources\SurveyResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Support\Enums\MaxWidth;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Js;
 
 class EditSurvey extends EditRecord
 {
@@ -29,6 +31,12 @@ class EditSurvey extends EditRecord
     public function getTitle(): string | Htmlable
     {
         return $this->getRecordTitle();
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return parent::getCancelFormAction()
+            ->alpineClickHandler('window.location.href = ' . Js::from(static::getResource()::getUrl()));
     }
 
     protected function getHeaderActions(): array
