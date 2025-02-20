5bcb848b01cd488695fcdd598ec095cf21064a60
Iliyas M <im.smiliyas@gmail.com>
2025-02-20 10:39:04 +0530

    Add tab-based filtering to ManageMeetings page
    Introduced a `getTabs` method with filters for meeting states such as Draft, Published, Voting in Progress, and Completed. Each tab includes specific query conditions to streamline meeting management and improve user navigation.

--- app/Filament/User/Resources/MeetingResource/Pages/ManageMeetings.php
+++ app/Filament/User/Resources/MeetingResource/Pages/ManageMeetings.php
@@ -4,7 +4,9 @@ namespace App\Filament\User\Resources\MeetingResource\Pages;
 
 use App\Filament\User\Resources\MeetingResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ManageMeetings extends ManageRecords
 {
@@ -14,6 +16,45 @@ class ManageMeetings extends ManageRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'draft' => Tab::make('Draft')
+                ->icon('heroicon-o-pencil-square')
+                ->query(
+                    fn (Builder $query) => $query->whereNull('cancelled_at')
+                        ->whereNull('published_at')
+                ),
+            'published' => Tab::make('Published')
+                ->icon('heroicon-o-rocket-launch')
+                ->query(
+                    fn (Builder $query) => $query->whereNull('cancelled_at')
+                        ->whereNotNull(columns: 'published_at')
+                        ->whereNull('voting_closed_at')
+                        ->where('voting_starts_at', '>=', now())
+                ),
+            'voting_in_progress' => Tab::make('Voting in progress')
+                ->icon('heroicon-o-chart-bar')
+                ->query(
+                    fn (Builder $query) => $query->whereNull('cancelled_at')
+                        ->whereNotNull(columns: 'published_at')
+                        ->whereNull('voting_closed_at')
+                        ->where('voting_starts_at', '<=', now())
+                ),
+            'completed' => Tab::make('Completed')
+                ->icon('heroicon-o-check-circle')
+                ->query(
+                    fn (Builder $query) => $query->whereNotNull('cancelled_at')
+                        ->orWhere(
+                            fn (Builder $query) => $query
+                                ->whereNotNull(columns: 'published_at')
+                                ->where('voting_closed_at', '<=', now())
+                        )
+                ),
         ];
     }
 }
