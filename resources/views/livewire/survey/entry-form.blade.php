48c654193e3ce5d32fecf0b5379fa841fe45d1f3
Iliyas M <im.smiliyas@gmail.com>
2025-04-28 16:13:00 +0530

    feat: add translate attribute to survey title
    Prevent text translation of the survey title by adding the "translate" attribute. This ensures the title remains consistent across all languages.

--- resources/views/livewire/survey/entry-form.blade.php
+++ resources/views/livewire/survey/entry-form.blade.php
@@ -2,6 +2,7 @@
     class="mx-auto flex w-full max-w-screen-md flex-col justify-center gap-y-6 px-4 py-6 sm:px-6 lg:px-8"
 >
     <h1
+        translate="no"
         class="text-center text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3xl"
     >
         {{ $survey->title }}
